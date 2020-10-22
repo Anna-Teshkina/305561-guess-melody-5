d5a292aa92113c2e55218f0d4ddffed12c114435
Anna Teshkina <anna_teshkina@mail.ru>
2020-10-22 17:07:30 +0300

    Опишем метод render у компонента GameScreen.

--- src/components/game-screen/game-screen.jsx
+++ src/components/game-screen/game-screen.jsx
@@ -2,7 +2,12 @@
 // Компонент должен рендериться по маршруту `/game`.
 // В стейте будем хранить индекс очередной игры.
 
-import {PureComponent} from "react";
+import React, {PureComponent} from "react";
+import PropTypes from 'prop-types';
+import {Redirect} from 'react-router-dom';
+import {GameType} from '../../const';
+import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
+import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
 
 class GameScreen extends PureComponent {
   constructor(props) {
@@ -12,6 +17,51 @@ class GameScreen extends PureComponent {
       step: 0,
     };
   }
+
+  render() {
+    const {questions} = this.props;
+    const {step} = this.state;
+    const question = questions[step];
+
+    if (step >= questions.length || !question) {
+      return (
+        <Redirect to="/" />
+      );
+    }
+
+    switch (question.type) {
+      case GameType.ARTIST:
+        return (
+          <ArtistQuestionScreen
+            question={question}
+            onAnswer={() => {
+              this.setState((prevState) => ({
+                step: prevState.step + 1,
+              }));
+            }}
+          />
+        );
+
+      case GameType.GENRE:
+        return (
+          <GenreQuestionScreen
+            question={question}
+            onAnswer={() => {
+              this.setState((prevState) => ({
+                step: prevState.step + 1,
+              }));
+            }}
+          />
+        );
+    }
+
+    return <Redirect to="/" />;
+  }
+
 }
+
+GameScreen.propTypes = {
+  questions: PropTypes.array.isRequired,
+};
 
 export default GameScreen;
